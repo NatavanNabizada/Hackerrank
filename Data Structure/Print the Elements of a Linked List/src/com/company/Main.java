ced6cb4caaec105deda885c79b3e6544be0fbc91
Natavan Nabizade <55505907+nnatevan@users.noreply.github.com>
2021-02-12 11:04:53 +0300

    Update Main.java

--- Data Structure/Print the Elements of a Linked List/src/com/company/Main.java
+++ Data Structure/Print the Elements of a Linked List/src/com/company/Main.java
@@ -1,41 +1,5 @@
 package com.company;
-
-import java.util.*;
-
 public class Main {
-
-    static class SinglyLinkedListNode {
-        public int data;
-        public SinglyLinkedListNode next;
-
-        public SinglyLinkedListNode(int nodeData) {
-            this.data = nodeData;
-            this.next = null;
-        }
-    }
-
-    static class SinglyLinkedList {
-        public SinglyLinkedListNode head;
-        public SinglyLinkedListNode tail;
-
-        public SinglyLinkedList() {
-            this.head = null;
-            this.tail = null;
-        }
-
-        public void insertNode(int nodeData) {
-            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
-
-            if (this.head == null) {
-                this.head = node;
-            } else {
-                this.tail.next = node;
-            }
-
-            this.tail = node;
-        }
-    }
-
     /*
      * For your reference:
      *
@@ -45,32 +9,11 @@ public class Main {
      * }
      *
      */
-    //MAIN PART
     static void printLinkedList(SinglyLinkedListNode head) {
 
         while (head != null) {
             System.out.println(head.data);
             head = head.next;
         }
-    }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        SinglyLinkedList llist = new SinglyLinkedList();
-
-        int llistCount = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < llistCount; i++) {
-            int llistItem = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            llist.insertNode(llistItem);
-        }
-
-        printLinkedList(llist.head);
-
-        scanner.close();
     }
 }
